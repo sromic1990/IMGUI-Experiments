d40ab61faa8692574f96c2552bf9fe5b309a782c
Sourav Chatterjee <sromic@gmail.com>
2023-09-02 22:51:31 +0300

    Updated Main to ensure all libaries work. Added render.h and render.cpp as a helper class to draw ImGui

--- IMGUI-Experiments/src/Main.cpp
+++ IMGUI-Experiments/src/Main.cpp
@@ -1,87 +1,137 @@
-#include <iostream>
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include "imgui.h"
+#include <cstdint>
+#include <filesystem>
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "imgui_impl_opengl3_loader.h"
+#include <imgui.h>
+#include <implot.h>
 
-int main()
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>
+
+#include "render.h"
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1900) &&                                 \
+    !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
+#pragma comment(lib, "legacy_stdio_definitions")
+#endif
+
+constexpr auto WINDOW_WIDTH = std::uint32_t{ 1280 };
+constexpr auto WINDOW_HEIGHT = std::uint32_t{ 720 };
+
+namespace fs = std::filesystem;
+
+static void glfw_error_callback(int error, const char* description);
+
+void start_cycle()
 {
-	if(!glfwInit())
-	{
-		return -1;
-	}
+    glfwPollEvents();
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+}
 
-	//Creating a windowed mode window and its OpenGL context
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, World", NULL, NULL);
-	if(!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
+void end_cycle(GLFWwindow* const window)
+{
+    const auto clear_color =
+        ImVec4(30.0F / 255.0F, 30.0F / 255.0F, 30.0F / 255.0F, 1.00f);
+    int display_w = 0;
+    int display_h = 0;
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x * clear_color.w,
+        clear_color.y * clear_color.w,
+        clear_color.z * clear_color.w,
+        clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+}
 
-	//Make window's context current
-	glfwMakeContextCurrent(window);
+int main(int, char**)
+{
+    // Setup window
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit())
+    {
+        return 1;
+    }
 
-	//Initialize GLEW
-	if(glewInit() != GLEW_OK)
-	{
-		return -1;
-	}
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+#endif
 
-	//Setup ImGui context
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
+    // Create window with graphics context
+    auto* window = glfwCreateWindow(static_cast<std::int32_t>(WINDOW_WIDTH),
+        static_cast<std::int32_t>(WINDOW_HEIGHT),
+        "Gui",
+        nullptr,
+        nullptr);
+    if (window == nullptr)
+    {
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
 
-	//Setup Platform/renderer bindings
-	ImGui_ImplGlfw_InitForOpenGL(window, true);
-	ImGui_ImplOpenGL3_Init("#version 130");
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
 
-	//Setup ImGui Style
-	ImGui::StyleColorsDark();
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
 
-	//Main Loop
-	while(!glfwWindowShouldClose(window))
-	{
-		//Poll for and process events
-		glfwPollEvents();
+    auto& style = ImGui::GetStyle();
+    style.Colors[ImGuiCol_TableBorderStrong] = ImVec4(1.0, 1.0, 1.0, 1.0);
+    style.Colors[ImGuiCol_TableBorderLight] = ImVec4(1.0, 1.0, 1.0, 1.0);
 
-		//Start ImGui frame
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
+    WindowClass window_obj;
 
-		//Create an ImGui window
-		ImGui::Begin("Hello, World");
-		ImGui::Text("This is some useful text!");
-		ImGui::End();
+    while (!glfwWindowShouldClose(window))
+    {
+        start_cycle();
 
-		glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::NewFrame();
+        ImPlot::CreateContext();
+        render(window_obj);
+        ImGui::Render();
 
-		//Draw a triangle
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();
+        end_cycle(window);
+    }
+    ImPlot::DestroyContext();
 
-		//Render ImGui
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		
-		//Swap front and back buffers
-		glfwSwapBuffers(window);
-	}
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
-	//Clean up ImGui
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
-	glfwTerminate();
+    return 0;
+}
 
-	std::cout << "Hello, World!" << std::endl;
-
-	return 0;
+void glfw_error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
